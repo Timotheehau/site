16af2e8db59b87114fa574b25b8a418a5e3124b1
Timothée Hauser <timon@MacBook-Air-de-Timothee.local>
2025-09-16 17:14:45 +0200

    ajout header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,22 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <img src="/assets/photo.jpg" alt="Timothée Hauser" className="avatar" />
-      <h1>Timothée Hauser</h1>
-      <p>Développeur Web | Étudiant en BTS SIO SLAM</p>
+      <img src="/assets/logo.png" alt="JhPrint3D" className="logo" />
+      <h1 className='title'>JhPrint3D</h1>
+      <div>
+        <nav>
+            <ul className="nav-links" >
+                <li><a href="#accueil">Accueil</a></li>
+                <li><a href="#produits">Produits</a></li>
+                <li><a href="#nouveautés">Nouveautés</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#idees"><img src="/assets/idees-icon.png" alt="Idées" className="icon" />Idées</a></li>
+            </ul>
+        </nav>
+    </div>
     </header>
   );
 }
+
 
 export default Header;
