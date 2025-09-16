e75dd6ebb78fa2db919eb5aa02d5b71165f27af0
Timothée Hauser <timon@MacBook-Air-de-Timothee.local>
2025-09-16 10:55:11 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <img src="/assets/photo.jpg" alt="Timothée Hauser" className="avatar" />
+      <h1>Timothée Hauser</h1>
+      <p>Développeur Web | Étudiant en BTS SIO SLAM</p>
+    </header>
+  );
+}
+
+export default Header;
